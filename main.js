0d0d808b52e095fcd2d618eeecb0f0bb30d8f6d8
brunnermanu <brunner.manu@hotmail.com>
2019-04-14 13:17:51 +0200

    mein zweiter push

---
+++ main.js
@@ -0,0 +1,24 @@
+//Get modal element
+var modal = document.getElementById('simpleModal');
+
+
+//get open modal button
+var modalButton = document.getElementById('modalButton');
+
+
+//get close button
+var closeButton = document.getElementsByClassName('closeButton')[0];
+
+
+//function to close Modal
+function closeModal(){
+    modal.style.display = 'none';
+    console.log('fired')
+}
+
+//listen for close click
+closeButton.addEventListener('click', closeModal);
+
+setTimeout(function(){ 
+    modal.style.display = 'block';    
+}, 60000);
