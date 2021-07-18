b6fdfc1a0c100f37e4485aed67759c13db5ab88c
brunnermanu <brunner.manu@hotmail.com>
2021-07-18 14:38:40 +0200

    add infobox and made header with flexbox

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 //Get modal element
-var modal = document.getElementById('simpleModal');
-
+let modal = document.getElementById('simpleModal');
 
 //get open modal button
-var modalButton = document.getElementById('modalButton');
+let modalButton = document.getElementById('modalButton');
 
 
 //get close button
-var closeButton = document.getElementsByClassName('closeButton')[0];
-
+let closeButton = document.getElementsByClassName('closeButton')[0];
 
 //function to close Modal
-function closeModal(){
+function closeModal() {
     modal.style.display = 'none';
     console.log('fired')
 }
 
 //listen for close click
 closeButton.addEventListener('click', closeModal);
+
 
 setTimeout(function(){ 
     modal.style.display = 'block';    
